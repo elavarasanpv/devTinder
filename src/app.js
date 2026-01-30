6f578baeec6d76465d58e88bdbd5e48c60ba984e
Elavarasan102 <100773690+Elavarasan102@users.noreply.github.com>
2026-01-30 10:32:39 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+/** @format */
+const express = require("express");
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+  res.send("Hello, hello!");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
